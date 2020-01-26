36aca410970a690bc7313f89a25152f7ad42fe5f
aranteslucas10 <lucas_arantes10@hotmail.com>
2020-01-26 15:34:43 -0300

    Atualiza arquivo de ocnfiguração

--- init.el
+++ init.el
@@ -1,50 +1,95 @@
-;; init.el --- Emacs configuration
+;; .emacs.d/init.el
 
-;; INSTALL PACKAGES
-;; --------------------------------------
-
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
 (require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 
+;; Adds the Melpa archive to the list of available repositories
 (add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
+;; Initializes the package infrastructure
 (package-initialize)
+
+;; If there are no archived package contents, refresh them
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+;; Installs packages
+;;
+;; myPackages contains a list of package names
 (defvar myPackages
-  '(better-defaults
-    ein
-    elpy
-    flycheck
-    material-theme
-    py-autopep8))
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    elpy                            ;; Emacs Lisp Python Environment
+    flycheck                        ;; On the fly syntax checking
+    py-autopep8                     ;; Run autopep8 on save
+    blacken                         ;; Black formatting on save
+    molokai-theme                  ;; Theme
+    )
+  )
 
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
 (mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
+          (unless (package-installed-p package)
+            (package-install package)))
       myPackages)
 
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
+;; ===================================
+;; Basic Customization
+;; ===================================
 
-(setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'molokai t)            ;; Load molokai theme
+(global-linum-mode t)               ;; Enable line numbers globally
 
-;; PYTHON CONFIGURATION
-;; --------------------------------------
+;; User-Defined init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (python-django linum-relative molokai-theme better-defaults)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
+;; ====================================
+;; Development Setup
+;; ====================================
+;; Enable elpy
 (elpy-enable)
-(elpy-use-ipython)
 
-;; use flycheck not flymake with elpy
+;; Enable Flycheck
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-;; enable autopep8 formatting on save
+;; Enable autopep8
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; init.el ends here
+;; User-Defined init.el ends here
