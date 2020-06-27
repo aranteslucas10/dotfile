d7c3b82019802de58a2873d91056aeba6bdc9984
aranteslucas10 <lucas_arantes10@hotmail.com>
2020-06-27 13:08:10 -0300

    Update wallpaper

--- wallpaper_from_nasa.py
+++ wallpaper_from_nasa.py
@@ -1,66 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Script para buscar imagens do dia do site da nasa e colocar como fundo
+# Download and set background image from the picture of the day
 # Author: Lucas Arantes <lucas_arantes10@hotmail.com>
 
 import requests
 import urllib
 import os
 import subprocess
+import argparse
 
 from bs4 import BeautifulSoup
 from datetime import date
 
 
-def set_image_backgroud(image_name):
-    """
-    Coloca a imagem como papel de parede.
-    """
-    subprocess.call(['feh', '--bg-scale', image_name])
+NASA_URL = "https://apod.nasa.gov/apod/"
 
 
-def download_imagem(url_imagem, destino):
-    """
-    Realiza o donwload da imagem na caminho de destino.
-    """
-    urllib.urlretrieve(url_imagem, destino)
+def set_image_backgroud(image_path):
+    """ Set a background image using feh """
+    subprocess.call(['feh', '--bg-scale', image_path])
 
 
-def get_url_imagem_from_nasa_url():
-    """
-    Busca a url da imagem do dia no site da nasa.
-    """
-    url_nasa = "https://apod.nasa.gov/apod/"
-    resp = requests.get(url_nasa)
+def image_download(url, path):
+    """ Download image in path """
+    urllib.urlretrieve(url, path)
+
+
+def get_image_url_from_nasa_url():
+    """ Find image url from nasa site """
+    response = requests.get(NASA_URL)
     try:
-        if resp.status_code == 200:
-            soup = BeautifulSoup(resp.content, 'html.parser')
-            image_link = soup.find(name="img").get_attribute_list("src")[0]
-            return "%s%s" % (url_nasa, image_link)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            link_image = soup.find(name="img").get_attribute_list("src")[0]
+            return "%s%s" % (NASA_URL, link_image)
     except AttributeError:
-        print("Sem imagens para o dia de hoje.")
+        print("No image today.")
         return None
 
 
-def main():
-    """
-    Função principal, busca, baixa e colcoa como papel de parede a
-    'Picture of the day' disponível no site da nasa.
-    """
-    image_path = "/home/lucas/Imagens/nasa_imagens"
+def main(local_path):
+    """ Main function, find, download, and set background image """
     current_date = date.today()
-    path_image_name = "%s/imagem_%s.jpg" % (image_path,
+    path_image_name = "%s/imagem_%s.jpg" % (local_path,
                                             current_date.strftime("%Y%m%d"))
     if not os.path.exists(path_image_name):
-        image_url = get_url_imagem_from_nasa_url()
-        if image_url:
-            download_imagem(image_url, path_image_name)
+        url_image = get_image_url_from_nasa_url()
+        if url_image:
+            image_download(url_image, path_image_name)
         else:
-            path_image_name = image_path
-
+            path_image_name = local_path
     set_image_backgroud(path_image_name)
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, required=True,
+                    dest="path", help="local path to put the images")
+
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args.path)
